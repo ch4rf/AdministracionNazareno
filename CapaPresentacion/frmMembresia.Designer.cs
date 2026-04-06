d9a79ea66ad1465f358682b83071a72e5c32971b
Jose-C25 <chubj690@gmail.com>
2026-04-05 21:15:32 -0600

    Cambios en las propiedades del form membresia capa presentacion

--- CapaPresentacion/frmMembresia.Designer.cs
+++ CapaPresentacion/frmMembresia.Designer.cs
@@ -34,8 +34,9 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1479, 831);
             Name = "frmMembresia";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "MEMBRESIA";
             WindowState = FormWindowState.Maximized;
             ResumeLayout(false);
